4032b1a21764d1a07e3206afe37d307daffa11ec
Marc Hurabielle <marc.hurabielle@gmail.com>
2017-10-03 12:39:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Redux-Promise-Observable
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import Rx from 'rxjs';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { combineEpics } from 'redux-observable';
+
+const SUCCESS = 'SUCCESS'
+const FAILURE = 'FAILURE'
+
+const reduxFormObservable$ = new BehaviorSubject(combineEpics())
+
+export const rxFromObservableEpic = (actions$, store) =>
+  reduxFormObservable$.mergeMap(epic =>
+    epic(actions$, store),
+  )
+
+const createRxFormEpic = (listenner, rxObserver) =>
+  action$ =>
+    action$.ofType(listenner[0])
+      .do(_ => rxObserver.next(SUCCESS))
+      .race(
+        action$.ofType(listenner[1])
+          .do(_ => rxObserver.next(FAILURE)),
+      ).ignoreElements()
+
+
+export const rxFormActionCreator = (action, listenner) => {
+  const rxObservable = new Rx.Observable(rxObserver => {
+    reduxFormObservable$.next(createRxFormEpic(listenner, rxObserver))
+  })
+  let rxSubscriber
+  return (payload, dispatch) => {
+    return new Promise((resolve, reject) => {
+      dispatch(action(({...payload})))
+      rxSubscriber = rxObservable.subscribe((m) => {
+        console.log("couou");
+        if (m === SUCCESS) {
+          resolve()
+        } else {
+          reject()
+        }
+      })
+    }).then(_ => rxSubscriber.unsubscribe())
+  }
+}
